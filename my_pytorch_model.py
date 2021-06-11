a77d7957b3ef65275a93480c8498f394423ea68a
Michael Zhang <mzzhang2014@gmail.com>
2021-06-11 15:16:40 -0700

    Somewhat working

--- my_pytorch_model.py
+++ my_pytorch_model.py
@@ -22,7 +22,7 @@ F_UV = 118 / 0.0719**2
 H_to_He = 9
 gamma = 5./3
 
-length_scale = 1e9
+length_scale = Rp
 vel_scale = 1e6
 rho_scale = 1e-12
 mass_scale = rho_scale * length_scale**3
@@ -44,12 +44,30 @@ def get_derivs(r, vals):
     derivs = torch.zeros(len(vals))
     derivs[1:-1] = (vals[2:] - vals[:-2]) / (r[2:] - r[:-2])
     return derivs
-        
-def differential_equations(scaled_r, scaled_v, scaled_rho, fion, scaled_T, margin=5):
+
+def padded_diff(vals):
+    result = np.diff(vals)
+    result = np.append(0, result)
+    return result
+
+def differential_equations(scaled_r, scaled_v, scaled_rho, scaled_fion, scaled_T, margin=5):
+    '''coeffs = np.polyfit(1/scaled_r.detach(), np.log(scaled_rho.detach()), 10)
+    predicted = np.polyval(coeffs, 1/scaled_r)
+    plt.plot(scaled_r.detach(), np.log(scaled_rho.detach()))
+    plt.plot(scaled_r.detach(), predicted)
+    plt.show()'''
+    
     r = scaled_r * length_scale
-    v = scaled_v * vel_scale
-    rho = scaled_rho * rho_scale / scaled_r**2
-    T = scaled_T * T_scale
+    v = torch.cumsum(scaled_v * vel_scale, 0) + 400
+    rho = torch.exp(torch.cumsum(scaled_rho, 0) + 4.809) * rho_scale / scaled_r**2
+    fion = torch.cumsum(scaled_fion, 0)
+    T = torch.cumsum(scaled_T * T_scale, 0) + 1173
+
+    '''plt.loglog(r.detach() / Rp, v.detach())
+    plt.loglog(r.detach() / Rp, rho.detach())
+    plt.loglog(r.detach() / Rp, fion.detach())
+    plt.loglog(r.detach() / Rp, T.detach())
+    plt.show()'''
     
     v_derivs = get_derivs(r, v)
     rho_derivs = get_derivs(r, rho)
@@ -80,12 +98,20 @@ def differential_equations(scaled_r, scaled_v, scaled_rho, fion, scaled_T, margi
     loss = (continuity_diffs**2 + momentum_diffs**2 + energy_diffs**2)[margin:-margin].sum()
 
     #Enforce inner boundary condition
-    loss += ((v[0:5] * 1e5)**2 + (T[0:5] - 1000)**2 / 0.01**2 + (fion[0:5] * 1e3)**2).sum()
+    #loss += ((v[0:5] * 1e5)**2 + (T[0:5] - 1000)**2 / 0.01**2 + (fion[0:5] * 1e3)**2).sum()
     
     #if torch.isnan(loss):
     #    import pdb
     #    pdb.set_trace()
-    
+
+    '''plt.figure()
+    plt.plot(r.detach() / Rp, continuity_diffs.detach())
+    plt.plot(r.detach() / Rp, momentum_diffs.detach())
+    plt.plot(r.detach() / Rp, energy_diffs.detach())
+    plt.show()'''
+    if torch.isinf(loss):
+        loss *= 0
+        loss += 1e9
     return loss
     
     #plt.loglog(r / Rp, r**2 * rho * v / (mass_scale / length_scale / time_scale))
@@ -127,29 +153,40 @@ cooling = np.interp(radii, 14.9 * Rp - data[:,0], data[:,2])
 dtype = torch.float64
 device = torch.device("cpu")
 tensor_radii = torch.tensor(radii / length_scale, device=device, dtype=dtype)
-wind_vel = torch.tensor(wind_vel / vel_scale, device=device, dtype=dtype, requires_grad=True)
-rho = torch.tensor(rho / rho_scale * (radii / length_scale)**2, device=device, dtype=dtype, requires_grad=True)
-fion = torch.tensor(np.copy(HII), device=device, dtype=dtype, requires_grad=True)
-#T = torch.tensor(Te.copy() / T_scale, device=device, dtype=dtype, requires_grad=True)
-T = torch.tensor(np.ones(len(radii)), device=device, dtype=dtype, requires_grad=True)
+wind_vel = torch.tensor(padded_diff(wind_vel / vel_scale), device=device, dtype=dtype, requires_grad=True)
+rho = torch.tensor(padded_diff(np.log(rho / rho_scale * (radii / length_scale)**2)), device=device, dtype=dtype, requires_grad=True)
+fion = torch.tensor(padded_diff(np.copy(HII)), device=device, dtype=dtype, requires_grad=True)
+#T = torch.tensor(padded_diff(Te.copy()) / T_scale, device=device, dtype=dtype, requires_grad=True)
+T = torch.tensor(np.zeros(len(radii)), device=device, dtype=dtype, requires_grad=True)
 minimum_loss = np.inf
-optimizer = torch.optim.Adam([wind_vel, rho, fion, T], lr=1e-4, eps=1e-8)
+parameters = [wind_vel, rho, fion, T]
+optimizer = torch.optim.Adam(parameters, lr=1e-6, eps=1e-4)
 
-for t in range(int(1e5)):
+for t in range(int(1e4)):
     optimizer.zero_grad()    
     loss = differential_equations(tensor_radii, wind_vel, rho, fion, T)
     if loss.item() < minimum_loss and not np.isnan(loss.item()):
         minimum_loss = loss.item()
-        best_yet = [wind_vel, rho, fion, T]
+        best_yet = [torch.clone(wind_vel), torch.clone(rho), torch.clone(fion), torch.clone(T)]
 
-    if loss.item() < 1: break
+    #if loss.item() < 1: break
         
     #print(loss.item())
-    if t % 100 == 0:
+    if t % 1 == 0:
         print(t, loss.item())
     loss.backward()
+    if t % 1000 == 0:
+        print(torch.min(wind_vel.grad).detach().numpy(),
+              torch.max(wind_vel.grad).detach().numpy(),
+              torch.min(rho.grad).detach().numpy(),
+              torch.max(rho.grad).detach().numpy(),
+              torch.min(fion.grad).detach().numpy(),
+              torch.max(fion.grad).detach().numpy(),
+              torch.min(T).detach().numpy(),
+              torch.max(T).detach().numpy())
+    torch.nn.utils.clip_grad_norm_(parameters, max_norm=1e4)
     optimizer.step()
     
-
-plt.plot(radii, best_yet[3].detach().numpy())
+print(minimum_loss)
+plt.plot(radii, 1100 + T_scale * np.cumsum(best_yet[3].detach().numpy()))
 plt.show()
